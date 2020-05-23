4d9e2fe2f732a6c9ba9d2cdcbe116edee7a522cf
Jose Endrinal <francis.endrinal@gmail.com>
2020-05-24 06:47:54 +0800

    Generate setup script in current directory

--- R/setup.R
+++ R/setup.R
@@ -1,3 +1,21 @@
+#' This function sets up the reporting containing the analysis
+#'
+#' @description FillIn
+#'
+#' @param report_year
+#' @param report_month
+#'
+#' @importFrom fs path
+#' @importFrom usethis use_directory use_template
+#' @export
 setup <- function(report_year, report_quarter) {
-  usethis::use_directory(fs::path("reports", report_year, report_quarter))
+  report_path <- fs::path("reports", report_year, paste0("Q", report_quarter))
+  usethis::use_directory(report_path)
+
+  usethis::use_template(template = "setup.R",
+                        save_as = paste0(report_path, "/01-setup.R"),
+                        data = list(report_year = report_year,
+                                    report_quarter = report_quarter),
+                        package = "tqregreport",
+                        open = TRUE)
 }
